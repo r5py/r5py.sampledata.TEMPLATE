e45ee401266043796a7b75f0751fe693ab3e1d7f
Christoph Fink <christoph.fink@gmail.com>
2023-08-25 16:07:31 +0300

    remove r5py from dependencies

--- src/r5py/sampledata/CHANGE_THIS/__init__.py
+++ src/r5py/sampledata/CHANGE_THIS/__init__.py
@@ -4,20 +4,29 @@
 """Sample data set for r5py, covering CHANGE_THIS, downloaded upon first access."""
 
 
-from r5py.util.data_set import DataSet
+import warnings
 
 
-__all__ = [
-    "__version__",
-    "example",
-]
-
-__version__ = "0.1.0"
+__version__ = "0.1.1.dev0"
+__all__ = ["__version__"]
 
 
-BASE_URL = f"https://github.com/r5py/CHANGE_THIS/raw/v{__version__}/data/"
+try:
+    from r5py.util.data_set import DataSet
 
-example = DataSet(
-    f"{BASE_URL}/example.zip",
-    "57d0d5f3359cbd0d42cc7467c51ec9b14e0c9dc1665308246644fbc3bddd9a1f",
-)
+    BASE_URL = f"https://github.com/r5py/CHANGE_THIS/raw/v{__version__}/data/"
+
+    example = DataSet(
+        f"{BASE_URL}/example.zip",
+        "57d0d5f3359cbd0d42cc7467c51ec9b14e0c9dc1665308246644fbc3bddd9a1f",
+    )
+
+    __all__ += [
+        "example",
+    ]
+
+except ImportError:
+    warnings.warn(
+        "Install r5py to use the sample data sets",
+        RuntimeWarning,
+    )
