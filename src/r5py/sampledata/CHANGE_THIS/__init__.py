7c1f7737a5a430f2bc331cc889787469895ecbfe
Christoph Fink <christoph.fink@gmail.com>
2023-10-19 11:43:02 +0300

    update template to use `r5py.util.SampleDataSet`

--- src/r5py/sampledata/CHANGE_THIS/__init__.py
+++ src/r5py/sampledata/CHANGE_THIS/__init__.py
@@ -7,16 +7,16 @@
 import warnings
 
 
-__version__ = "0.1.1.dev0"
+__version__ = "0.1.1.dev1"
 __all__ = ["__version__"]
 
 
 try:
-    from r5py.util.data_set import DataSet
+    from r5py.util.sample_data_set import SampleDataSet
 
     BASE_URL = f"https://github.com/r5py/CHANGE_THIS/raw/v{__version__}/data/"
 
-    example = DataSet(
+    example = SampleDataSet(
         f"{BASE_URL}/example.zip",
         "57d0d5f3359cbd0d42cc7467c51ec9b14e0c9dc1665308246644fbc3bddd9a1f",
     )
@@ -27,6 +27,6 @@ try:
 
 except ImportError:
     warnings.warn(
-        "Install r5py to use the sample data sets",
+        "Install r5py>=0.1.1.dev1 to use the sample data sets",
         RuntimeWarning,
     )
